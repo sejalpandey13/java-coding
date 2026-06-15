da8af8a80852da2ddc14e822c51d565bd68f6f85
sejalpandey13 <sejalpandey1312@gmail.com>
2026-06-15 14:09:37 +0530

    updated code

--- 01_IsPrime.java
+++ 01_IsPrime.java
@@ -1,23 +1,59 @@
+import java.util.Scanner;
+
 class IsPrime {
-    public static boolean isPrime(int n) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int isPrime = 1; // 1 = Prime, 0 = Not Prime
+
         if (n <= 1) {
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
+            isPrime = 0;
+        } else {
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    isPrime = 0;
+                    break;
+                }
             }
         }
-        return true;
-    }
 
-    public static void main(String[] args) {
-        int number = 29; // Example number
-        if (isPrime(number)) {
-            System.out.println(number + " is a prime number.");
+        if (isPrime == 1) {
+            System.out.println("Prime");
         } else {
-            System.out.println(number + " is not a prime number.");
+            System.out.println("Not Prime");
         }
+
+        sc.close();
     }
-    
 }
+
+//Optimized Approach
+/*
+class IsPrime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int isPrime = 1; // 1 = Prime, 0 = Not Prime
+
+        if (n <= 1) {
+            isPrime = 0;
+        } else {
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0) {
+                    isPrime = 0;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime == 1) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not Prime");
+        }
+
+        sc.close();
+    }
+}*/
---
+++ 02_IsPrimeRange.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+//Brute Force Approach
+
+class IsPrimeRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for(int i = 0; i< t; i++){
+            int n = sc.nextInt();
+
+            int count = 0;
+            for(int j = 1; j <= n; j++){
+                if(n % j == 0){
+                    count++;
+                }
+            }       
+
+            if(count == 2){
+                System.out.println("Prime");
+            } else {
+                System.out.println("Not Prime");
+            }
+
+        }
+    }
+    
+}
+
+//Optimized Approach
+/*
+class IsPrimeRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+
+            int count = 0;
+
+            if (n <= 1) {
+                count = 1; // not prime
+            } else {
+                for (int j = 2; j <= Math.sqrt(n); j++) {
+                    if (n % j == 0) {
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            if (count == 0) {
+                System.out.println("Prime");
+            } else {
+                System.out.println("Not Prime");
+            }
+        }
+
+        sc.close();
+    }
+}*/
